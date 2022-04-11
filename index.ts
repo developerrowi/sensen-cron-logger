94d6ad52d0a2fbe62f98cde9af12e708b12e65a5
roweldc <roweldelacruz@solaireresort.com>
2022-04-11 19:51:45 +0800

    Init Update Sensen Consumer

---
+++ index.ts
@@ -0,0 +1,60 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import Main from './sensen/main';
+import { PubSub } from '@google-cloud/pubsub';
+import fs from 'fs'
+
+require('dotenv').config()
+
+let handlers = new Main()
+
+const app = express()
+
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
+
+const secureMiddleware = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    // console.log(req.headers.authorization)
+
+    if(!req.headers) return res.status(403).json({ error: 'Missing credentials' });
+    let authorization: any = req.headers.authorization?.split(' ');
+    const buff = Buffer.from(authorization[1], 'base64');
+    let decodedAuth = buff.toString();
+
+    if (decodedAuth === `${process.env.SENSEN_CONSUMER_USERNAME}:${process.env.SENSEN_CONSUMER_PASSWORD}`) {
+        return next()
+    }
+
+    return res.status(403).json({ error: 'Unauthorized access' });
+}
+
+
+app.use(secureMiddleware)
+
+app.post('/', async (req, res) => {
+    try {
+        res.status(200).send(await handlers.postToRabbitMQ(req.body))
+
+    } catch(e) {
+        res.status(404)
+    }
+
+})
+
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!')
+})
+
+// PubSub
+var creds: any = process.env.ENVIRONMENT === 'development' ?  fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS_ENCODED!) : process.env.GOOGLE_APPLICATION_CREDENTIALS_ENCODED!
+const pubsubCredentials = Buffer.from(creds, 'base64').toString('utf-8');
+export const pubsub = new PubSub({ credentials: JSON.parse(pubsubCredentials) });
---
+++ sensen/main.ts
@@ -0,0 +1,25 @@
+import { pubsub } from "..";
+import pkg from "../package.json"
+
+export default class Main {
+
+        constructor() {
+
+        }
+
+        async postToRabbitMQ(data: any) {
+            // 'Post to rabbitMQ here'
+            try {
+                const publishId = await pubsub.topic(process.env.SENSEN_TOPIC!).publishMessage({data: Buffer.from(JSON.stringify(data)) });
+                console.log(data)
+                return {data}
+            } catch(e: any) {
+                throw new Error(e)
+            }
+
+        }
+
+        async mapperSensenToRabbitMQ(data: any) {
+            
+        }
+}
