260c15a4025f66430f76cf5366198dc2b9126744
roweldc <roweldelacruz@solaireresort.com>
2022-05-04 14:15:09 +0800

    Update payload

--- sensen/main.ts
+++ sensen/main.ts
@@ -11,16 +11,16 @@ export default class Main {
             let mainData = Buffer.from(JSON.stringify(data))
 
 
-            // let finalData: any = {
-            //     context: {
-            //         data: {
-            //             message: mainData
-            //         }
-            //     }
-            // }
+            let finalData: any = {
+                context: {
+                    data: {
+                        message: data
+                    }
+                }
+            }
 
-            const publishId = await pubsub.topic(process.env.SENSEN_TOPIC!).publishMessage({ data: mainData });
-            console.log(data)
+            const publishId = await pubsub.topic(process.env.SENSEN_TOPIC!).publishMessage({ data: Buffer.from(JSON.stringify(mainData)) });
+            // console.log(finalData)
             return { data }
         } catch (error: any) {
             throw error
