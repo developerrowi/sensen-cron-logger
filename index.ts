36d9ca64f731830ff35134f28f12678b5b9107b6
roweldc <roweldelacruz@solaireresort.com>
2022-05-11 12:38:18 +0800

    Update sensen logger

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 import cors from 'cors'
-import helmet from 'helmet'
 import morgan from 'morgan'
-import Main from './sensen/main';
-import { PubSub } from '@google-cloud/pubsub';
 import fs from 'fs'
-import bodyParser from 'body-parser';
 import winston from 'winston';
+import  cron  from 'node-cron'
+import moment from "moment"
+var sys = require('util')
+var exec = require('child_process').exec;
+
 
 require('dotenv').config()
 
-let handlers = new Main()
 
 // logger 
 let loggerOptions = {
@@ -19,57 +19,34 @@ let loggerOptions = {
     transports: [new winston.transports.Console()],
 };
 export const logger = winston.createLogger(loggerOptions);
-//
-// PubSub
-// var creds: any = process.env.NODE_ENV === 'development' ?  fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS_ENCODED!) : process.env.GOOGLE_APPLICATION_CREDENTIALS_ENCODED!
-// const pubsubCredentials = Buffer.from(creds, 'base64').toString('utf-8');
-// export const pubsub = new PubSub({ credentials: JSON.parse(pubsubCredentials) });
-export const pubsub = new PubSub();
-
-const app = express()
 
 
-app.use(helmet());
 
-app.use(cors());
 
-app.use(morgan('combined'));
+var cronJob =  (async () => {
 
-app.use(express.json({limit: '1000mb'}));
-
-app.use(express.urlencoded({limit: '1000mb'}));
-
-const secureMiddleware = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    // console.log(req.headers.authorization)
-    if (!req.headers || !req.headers.authorization) return res.status(403).json({ error: 'Missing credentials' });
-    let authorization: any = req.headers?.authorization?.split(' ');
-    const buff = Buffer.from(authorization[1], 'base64');
-    let decodedAuth = buff.toString();
-
-    if (decodedAuth === `${process.env.SENSEN_CONSUMER_USERNAME}:${process.env.SENSEN_CONSUMER_PASSWORD}`) {
-        return next()
-    }
-    return res.status(403).json({ error: 'Unauthorized access' });
-}
-
-// health check
-app.get('/', async (req, res) => {
-    res.status(200).json({
-        status: "OK",
-    })
-})
-
-app.use(secureMiddleware)
-
-app.post('/', async (req, res) => {
     try {
-        const rabbitMQSend = await handlers.postToPubSub(req.body)    
-        res.status(200).send(rabbitMQSend)
-    } catch (error: any) {
-        res.status(500).send({ error, step: "Error in publishing data to pubsub" });
-    }
-})
+       cron.schedule('0 0 */1 * * *', async () => {
+        
+  
+        //   logger.info({message: "Iloveyou"})
+          let manualTag: any = []
 
-app.listen(3000, () => {
-    console.log('The application is listening on port 3000!')
-})
+          const currentDate = new Date()  
+          let logName = moment(currentDate).format('YYYY-MM-DDTHH:mm:ss')
+          var ping = exec(`kubetail sensen-consumer -s 1h -n microservice | tee ${logName}-logs.txt`);
+          ping.stdout.on('data', (data: any) => {
+            console.log('' + data);
+          });
+
+
+ 
+      });
+    }
+    catch(e) {
+        console.log(e)
+    }
+
+  })
+
+cronJob()
